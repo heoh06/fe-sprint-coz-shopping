9175e4712ee9332423ff43191c62f79e37f767b3
heoh0613 <heoh0613@gmail.com>
2023-05-15 17:09:05 +0900

    card done

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ItemList from './components/ItemList'
 
 function App() {
   return (
     <>
     <Header/>
+    <section className='main_section_product'>
+      <h2>상품 리스트</h2>
+      <ItemList/>
+    </section>
+    <section className='main_section_bookmark'>
+      <h2>북마크 리스트</h2>
+    </section>
     <Footer/>
     </>
   );
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,2 +1,41 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+function Card() {
+    const [data, setData] = useState([]);
+	
+    useEffect(() => {
+        const fetchData = async() => {
+        const res = await axios.get('http://cozshopping.codestates-seb.link/api/v1/products');
+        console.log(res)
+        return res.data;
+        }	
+    
+        fetchData().then(res => setData(res));
+    }, []);
+
+
+    return(
+            <li className='card_box'>
+            <div className='img_wrap'>
+                <img src={data[0].image_url} alt={data.title} className='card_img' />
+                <button className='icon_bookmark bookmark_off'>    
+                </button>
+        </div>
+            <div className='card_titles'>
+                <h3 className='card_title'>{data[0].title}</h3>
+                <h3 className='card_title'>{data[0].brand_name}</h3>
+                <span className='card_subtitle'>{data[0].discountPercentage}</span>
+                <span className='card_subtitle'>{data[0].type === "Brand" ? "관심고객수" : ""}</span>
+            </div>
+            <div className='card_details'>
+                <p className='card_detail_left'>{data[0].subtitle}</p>
+                <p className='card_detail_right'>{data[0].price}</p>
+                <p className='card_detail_right'>{data[0].follower}</p>
+            </div>
+            </li>
+    )
+}
+
+export default Card;
